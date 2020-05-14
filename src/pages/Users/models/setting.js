280e225870059b585a15bb29bda1b570cb60be0f
Jayden Deng <dqy676081195@gmail.com>
2020-05-14 11:19:47 +0800

    恢复误删的用户models

---
+++ src/pages/Users/models/setting.js
@@ -0,0 +1,47 @@
+import { message } from 'antd';
+import { UploadFile } from '@/services/files';
+import { EditUser } from '@/services/users';
+
+export default {
+
+  namespace: 'setting',
+
+  state: {
+    routes: [{ breadcrumbName: '用户', path: '/users' }, { breadcrumbName: '个人设置' }],
+    avatarFile: null,
+    avatarPreview: '',
+  },
+
+  subscriptions: {
+    setup({ dispatch, history }) {
+    },
+  },
+
+  effects: {
+    * eSubmitUpdate({ id, payload }, { select, call, put }) {
+      try {
+        const { avatarFile } = yield select(state => state['setting']);
+        if (avatarFile) {
+          const formData = new FormData();
+          const { avatarFile } = yield select(state => state['setting']);
+          formData.append('file', avatarFile);
+          formData.append('folder_path', 'avatar');
+          const { data: { file_url } } = yield call(UploadFile, formData);
+          payload.avatar = file_url;
+        }
+        const { msg } = yield call(EditUser, id, payload);
+        message.success(msg);
+        yield put({ type: 'basicLayout/eGetMe' });
+        yield put({ type: 'rUpdateState', payload: { avatarFile: null, avatarPreview: '' } });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+
+  reducers: {
+    rUpdateState(state, { payload }) {
+      return { ...state, ...payload };
+    },
+  },
+};
