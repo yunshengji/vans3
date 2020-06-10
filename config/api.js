9ca4aa780e8a7513498926f1ca647becb4b1fc08
Jayden Deng <dqy676081195@gmail.com>
2020-06-11 02:39:41 +0800

    切换正式环境

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
 module.exports = {
-  prefix: 'https://api.crm.wanmingxing.com/api/v1',
-  // prefix: 'https://api.prod-crm.wanmingxing.com/api/v1',
+  // prefix: 'https://api.crm.wanmingxing.com/api/v1',
+  prefix: 'https://api.prod-crm.wanmingxing.com/api/v1',
 };
